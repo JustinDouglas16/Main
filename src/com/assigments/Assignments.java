c7f3f47cbdba64e9342ea2f72b898879acfa47d3
blackhathacker101 <justindouglas1738@gmail.com>
2021-08-05 19:13:49 -0300

    first commit

---
+++ src/com/assigments/Assignments.java
@@ -0,0 +1,25 @@
+package com.assigments;
+
+
+
+public class Assignments {
+
+	public static void main(String[] args) {
+		//TODO: // assignment 1:
+		// vul een array met 1 miljoen records (allemaal zelfde naam)
+		// wijzig steeds van 1 record (de laatste, de middelste en de eerste)  de naam
+		// zoek naar dit record en laat zien hoe lang dit duurt.
+
+		// assignments 2:
+		// vul een hashmap met 1 miljoen records, gebruik elke keer een unieke id, de naam mag steeds hetzelfde
+		// zijn of mag je veranderen als je dat voor elkaar krijgt
+		// meet hoe lang het duurt om een record op te vragen
+		// maak 1 record met een andere naam,
+		// meet ook hoe lang het duurt om dit record via zijn key op te vragen...
+		
+
+
+
+	}
+
+}
