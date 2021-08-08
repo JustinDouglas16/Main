9f6fe78211998c37d5da8364df1ba9679ca3b022
blackhathacker101 <justindouglas1738@gmail.com>
2021-08-08 15:00:18 -0300

    put sum code

--- src/com/assigments/Assignments.java
+++ src/com/assigments/Assignments.java
@@ -1,6 +1,6 @@
 package com.assigments;
 
-
+import java.util.HashMap;
 
 public class Assignments {
 
@@ -9,17 +9,24 @@ public class Assignments {
 		// vul een array met 1 miljoen records (allemaal zelfde naam)
 		// wijzig steeds van 1 record (de laatste, de middelste en de eerste)  de naam
 		// zoek naar dit record en laat zien hoe lang dit duurt.
-
+		
 		// assignments 2:
 		// vul een hashmap met 1 miljoen records, gebruik elke keer een unieke id, de naam mag steeds hetzelfde
 		// zijn of mag je veranderen als je dat voor elkaar krijgt
 		// meet hoe lang het duurt om een record op te vragen
 		// maak 1 record met een andere naam,
 		// meet ook hoe lang het duurt om dit record via zijn key op te vragen...
-		System.out.println("helloworld");
-
-
-
+		
+		HashMap <String, Integer> namesMap = new HashMap<>();
+		long i;
+		for (i = 0; i <= 1000000L; i++) {
+			namesMap.put("justin douglas", 1);
+			
+			System.out.println(namesMap);
+		}
+	
 	}
-
 }
+	
+
+
