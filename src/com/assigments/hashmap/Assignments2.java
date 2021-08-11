e8db3bb4390e0c754065e3b233b88727e309dea4
blackhathacker101 <justindouglas1738@gmail.com>
2021-08-11 12:28:16 -0300

    new code implements

--- src/com/assigments/hashmap/Assignments2.java
+++ src/com/assigments/hashmap/Assignments2.java
@@ -6,19 +6,19 @@ public class Assignments2 {
 
 	public static void main(String[] args) {
 		// assignments 2:
-		// vul een hashmap met 1 miljoen records, gebruik elke keer een unieke id, de naam mag steeds hetzelfde
 		// zijn of mag je veranderen als je dat voor elkaar krijgt
 		// meet hoe lang het duurt om een record op te vragen
-		// maak 1 record met een andere naam,
 		// meet ook hoe lang het duurt om dit record via zijn key op te vragen...
 		
 		HashMap <Integer, String> namesMap = new HashMap<>();		
+		// vul een hashmap met 1 miljoen records, gebruik elke keer een unieke id, de naam mag steeds hetzelfde
 		
 		for (int i = 1; i <= 1000; i++) 
 		{
+			// maak 1 record met een andere naam,
 		namesMap.put(i, "justin");
 		namesMap.put(1001, "gianni");
-			
+		namesMap.put(500, "george");
 		}
 		System.out.println(namesMap);
 	}
